5caa20491c9b887c5bad58f45a1c39bda0a2fcd6
angelo-mab <60443712+angelo-mab@users.noreply.github.com>
2021-04-08 12:34:50 -0400

    task module and project modules are being finished

---
+++ src/modules/projects.js
@@ -0,0 +1,62 @@
+import { toDate, isToday, isThisWeek, subDays } from 'date-fns';
+
+export default class Project {
+  // Each project requires to have a name and a list of task to initiate 
+  constructor(name) {
+    this.name = name;
+    this.tasks = [];
+  }
+
+  setName(name) {
+    this.name = name;
+  }
+  getName() {
+    return this.name;
+  }
+
+  setTasks(tasks) {
+    this.tasks = tasks;
+  }
+  getTasks() {
+    return this.tasks;
+  }
+
+  // This finds a task that matches with taskName
+  getTask(taskName) {
+    return this.tasks.find((task) => task.getName() === taskName);
+  }
+
+  contains(taskName) {
+    return this.tasks.some((task) => task.getName() === taskName);
+  }
+
+  // Adds a new task to the this.tasks array
+  addTask(task) {
+    if (this.tasks.indexOf(task) > 0) return;
+    this.tasks.push(task);
+  }
+
+  // Looks for a task of the same name,
+  // Assigns it to taskToDelete 
+  // Splices the index of taskToDelete and deletes just 1 entry
+  deleteTask(taskName) {
+    const taskToDelete = this.tasks.find((task) => task.getName() === taskName);
+    this.tasks.splice(this.tasks.indexOf(taskToDelete), 1);
+  }
+
+  // returns tasks due for today
+  // by filtering the 
+  getTasksToday() {
+    return this.tasks.filter((task) => {
+      const taskDate = new Date(task.getDateFormatted());
+      return isToday(toDate(taskDate));
+    });
+  }
+
+  getTasksThisWeek() {
+    return this.task.filter((task) => {
+      const taskDate = new Date(task.getDateFormatted());
+      return isThisWeek(subDays(toDate(taskDate), 1));
+    })
+  }
+}
